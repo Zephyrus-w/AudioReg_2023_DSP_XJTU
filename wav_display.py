4f28fb6dcc2dc57d4f9a340f2ab5637272ee4c35
Wang Xinye <97391559+Zephyrus-w@users.noreply.github.com>
2023-11-26 12:15:56 +0800

    Add files via upload

--- wav_display.py
+++ wav_display.py
@@ -1,9 +1,17 @@
 import soundfile as sf
 import matplotlib.pyplot as plt
 
+def average(list, start, length):#注意这里算的是绝对值
+    sum = 0
+    for i in range(length):
+        sum += abs(list[start + i])
+    return sum / length
+'''
+以下请修改文件名
+'''
 # 读取音频文件
-wavsignal, rt = sf.read('dataset/original/original_1.wav')
-
+wavsignal, rt = sf.read('dataset/original/original_2.wav')
+# rt是采样频率，wavsignal.shape[0]存储了音频的长度
 
 # 单通道音频
 length = wavsignal.shape[0]
